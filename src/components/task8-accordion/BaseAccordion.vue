22e4d9dcb6548fc0a9882b692e6d767abe69c8ba
Muhammad Saif Alli <muhammadsaifalli222@gmail.com>
2026-04-08 06:41:18 +0200

    Task 8 - Simple Accordion Component styling transitions

--- src/components/task8-accordion/BaseAccordion.vue
+++ src/components/task8-accordion/BaseAccordion.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="wrapper" @click="toggleOpenClose">
-    <div class="accordion-clicker">
+  <div class="wrapper">
+    <div class="accordion-clicker" @click="toggleOpenClose">
       An Accordion
-      <span>{{isOpen ? '▲' : '▼' }}</span>
+      <span style="color: white">{{isOpen ? '▲' : '▼' }}</span>
     </div>
-    <div class="accordion-visor">
-      <BaseSelect :class="isOpen ? 'base-select-open' : 'base-select-closed'"/>
+    <div class="accordion-visor" :class="isOpen ? 'base-select-open' : 'base-select-closed'">
+      <BaseSelect style="padding: 1rem;"/>
     </div>
   </div>
 
@@ -25,25 +25,58 @@ function toggleOpenClose()
 </script>
 
 <style scoped>
-.base-select-closed
-{
-  display: none;
-}
-.base-select-open
-{
-  border: solid 2px white;
-  /*TODO transition ;*/
-}
-
 .accordion-clicker
 {
   display: flex;
   align-items: center;
   justify-content: space-between;
+
+  background-color: red;
+  color: yellow;
+  padding: 1rem;
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
 }
 .accordion-clicker:hover
 {
   background-color: cornflowerblue;
   color: black;
+}
+
+/* Options transitioning */
+.accordion-visor
+{
+  overflow: hidden;
+  /* This will apply to close and open*/
+  transition: max-height 1s ease, opacity 2s ease;
+
+  /*
+  Using it this way will allow you to specify different times
+  for opening and closing appearances
+  */
+  transition-property: max-height, opacity;
+  transition-timing-function: ease;
+}
+.base-select-closed
+{
+  max-height: 0;
+  opacity: 0;
+  /*
+  Using it this way will allow you to specify different times
+  for opening and closing appearances
+  */
+  transition-duration: 0.2s; /* closing speed */
+}
+.base-select-open
+{
+  max-height: 200px; /* pick something bigger than content */
+  opacity: 1;
+
+  border: 2px solid grey;
+  /*
+  Using it this way will allow you to specify different times
+  for opening and closing appearances
+  */
+  transition-duration: 0.5s; /* opening speed */
 }
 </style>
