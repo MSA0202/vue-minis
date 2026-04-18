99d22fa67555285a7dc401802cd22c458a7d6b63
Muhammad Saif Alli <muhammadsaifalli222@gmail.com>
2026-04-18 06:46:14 +0200

    Task 12 - Light / Dark theme example

--- src/components/task12-lightdarktheme/LightDarkThemView.vue
+++ src/components/task12-lightdarktheme/LightDarkThemView.vue
@@ -1,19 +1,33 @@
-
-
 <template>
   <div class="ldt-wrapper">
-    <BCButton class="light-theme">Click To Switch Theme</BCButton>
-    <BaseProgressBar class="light-theme"/>
-    <BaseTextarea class="light-theme"/>
+    <BCButton @click="changeTheme" :class="currentTheme">Click To Switch Theme</BCButton>
+    <BaseProgressBar :class="currentTheme"/>
+    <BaseTextarea :class="currentTheme"/>
   </div>
 </template>
 
 <script setup lang="ts">
-
 import BCButton from "@/components/task1-basecounter/BCButton.vue";
 import BaseProgressBar from "@/components/task9-progressbar/BaseProgressBar.vue";
 import "@/styles.css"
 import BaseTextarea from "@/components/task3-basetextarea/BaseTextarea.vue";
+import {ref} from "vue";
+
+const isWhite = ref(true);
+const currentTheme = ref("light-theme");
+function changeTheme()
+{
+  if (isWhite.value)
+  {
+    currentTheme.value = "dark-theme";
+    isWhite.value = false;
+  }
+  else
+  {
+    currentTheme.value = "light-theme";
+    isWhite.value = true;
+  }
+}
 </script>
 
 <style scoped>
@@ -21,6 +35,7 @@ import BaseTextarea from "@/components/task3-basetextarea/BaseTextarea.vue";
 {
   background-color: var(--light);
   color: var(--dark);
+  transition: 1s ease;
 }
 
 .light-theme:hover
@@ -33,9 +48,10 @@ import BaseTextarea from "@/components/task3-basetextarea/BaseTextarea.vue";
 {
   background-color: var(--dark);
   color: var(--light);
+  transition: 1s ease;
 }
 
-dark-theme:hover
+.dark-theme:hover
 {
   background-color: var(--light);
   color: var(--dark);
